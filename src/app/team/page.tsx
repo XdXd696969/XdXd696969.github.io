fa77d59fe0c48fe012777e02dac885f9ac677012
Muhammad Subhan Khan <msk97.sk@gmail.com>
2025-05-01 19:59:27 +0500

    pages initial setup

---
+++ src/app/team/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function TeamPage() {
+  return (
+    <main className="min-h-screen">
+      <div className="container mx-auto px-4 py-8">
+        <h1 className="text-4xl font-bold mb-8">Our Team</h1>
+        {/* Team content will be added here based on Figma design */}
+      </div>
+    </main>
+  );
+}
