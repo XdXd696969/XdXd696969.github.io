82c2324937d5fd190a56f33c7f194440745200cf
Muhammad Subhan Khan <msk97.sk@gmail.com>
2025-05-01 20:35:25 +0500

    home page design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Home() {
   return (
-    <main className="min-h-screen">
-      <div className="container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold mb-8">Welcome to DNN</h1>
-        {/* Home page content will be added here based on Figma design */}
+    <main className="flex items-center min-h-[88vh] p-8 ">
+      <div className="w-[55%] justify-center flex ">
+        <Image className="" src="/logo.png" alt="DNN Logo" width={900} height={1000} />
+      </div>
+      <div>
+        <p className="text-[95px] font-bold uppercase tracking-wide leading-tight bg-gradient-to-r from-[#FFFF] to-[#FFDA97] bg-clip-text text-transparent">
+          We are <br /> the media
+          <br /> now
+        </p>
+        <button className="border px-4 py-2 rounded-full text-lg mt-6 cursor-pointer hover:scale-105 transition-all duration-300">
+          Coming Soon
+        </button>
       </div>
     </main>
   );
